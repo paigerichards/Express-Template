fdfa395435a63045d1a24aa7c228e3893c9f3fc5
paigerichards <paige.richards14@hotmail.co.uk>
2017-06-15 11:32:56 +1000

    Express Template

---
+++ app.js
@@ -0,0 +1,9 @@
+// import express
+const express = require('express')
+// Create Express app
+const app = express()
+
+// Start web server (port)
+app.listen(7000, () => {
+  console.log('Server started at localhost:7000')
+})
