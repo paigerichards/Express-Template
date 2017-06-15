e8130cbc2b7516c86e12e722eb996faabdebb331
paigerichards <paige.richards14@hotmail.co.uk>
2017-06-15 11:44:08 +1000

    GET Request

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require('express')
 // Create Express app
 const app = express()
 
+//When GET/links is received
+app.get('/links',(req, res) => {
+  res.json({
+    items: [
+      'First',
+      'Second'
+    ]
+  })
+})
+
 // Start web server (port)
 app.listen(7000, () => {
   console.log('Server started at localhost:7000')
