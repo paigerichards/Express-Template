67e90a60265d16e86ff3a013072fb825dfcc5bf5
paigerichards <paige.richards14@hotmail.co.uk>
2017-06-15 12:04:47 +1000

    adding links

--- links.js
+++ links.js
@@ -4,6 +4,24 @@ const linksRouter = require('./routes/links')
 //Make a new Express Router
 const router = express.Router()
 
+const links = [
+    {
+        title: 'ProgrammableWeb API Directory',
+        url: 'https://www.programmableweb.com/apis/directory',
+        tags: ['api']
+    },
+    {
+        title: 'Presentational and Container Components',
+        url: 'https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0',
+        tags: ['react', 'component']
+    },
+    {
+        title: 'Traversing Twists and Turns',
+        url: 'https://medium.com/@vicalfieri/traversing-twists-and-turns-c298d9d4636',
+        tags: ['vic', 'blog']
+    }
+]
+
 //When GET/links is received
 app.get('/photos',(req, res) => {
   res.json({
